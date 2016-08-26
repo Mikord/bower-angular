0b041ce85c3371a6dab130300bd8640c7ce79711
a.petrov <petrov.anton@cg.ru>
2016-08-26 15:59:43 +0300

    delete rootScope apply from timeout function

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.8
+ * @license AngularJS v1.5.8-cg1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -19618,7 +19618,7 @@ function $TimeoutProvider() {
           delete deferreds[promise.$$timeoutId];
         }
 
-        if (!skipApply) $rootScope.$apply();
+        // if (!skipApply) $rootScope.$apply();
       }, delay);
 
       promise.$$timeoutId = timeoutId;
